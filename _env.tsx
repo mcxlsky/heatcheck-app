a866a5df4047267c95c15736e90c354811cad9fe
Ryan McCaulsky <46874692+mcxlsky@users.noreply.github.com>
2025-08-17 12:56:16 -0400

    Update 1

--- _env.tsx
+++ _env.tsx
@@ -1,11 +1,1 @@
-# YouTube Data API v3 Key
-# Get your API key from: https://console.developers.google.com/
-# 1. Create a new project or select existing one
-# 2. Enable YouTube Data API v3
-# 3. Create credentials (API Key)
-# 4. Replace "your_youtube_api_key_here" with your actual API key
-
-VITE_YOUTUBE_API_KEY=your_youtube_api_key_here
-
-# App Environment
-VITE_APP_ENV=development
+// This file was removed to fix build errors
